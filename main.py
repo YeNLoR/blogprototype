ad6072faae74f27edbe72d0a72f8864a0b4c86af
yenlor <avatarcik100@gmail.com>
2025-10-31 15:13:09 +0300

    Şifre Hashleme

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import flask
 import flask_sqlalchemy
 import flask_login
 import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask import request
+
 
 app = flask.Flask(__name__)
 
@@ -20,7 +22,7 @@ class Users(db.Model, flask_login.UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(32), unique=True)
     username = db.Column(db.String(16), nullable=False)
-    password = db.Column(db.String(16), nullable=False)
+    password = db.Column(db.String, nullable=False)
     register_date = db.Column(db.DateTime, nullable=False)
     posts = db.relationship('Posts', backref='author', lazy='dynamic')
     comments = db.relationship('Comments', backref='author', lazy='dynamic')
@@ -66,7 +68,7 @@ def login():
         password = request.form['password']
 
         user = Users.query.filter_by(username=username).first()
-        if user and password == user.password:
+        if user and check_password_hash(user.password, password):
             flask_login.login_user(user, remember=True)
             return flask.redirect(flask.url_for('profile'))
 
@@ -89,7 +91,7 @@ def register():
             u = Users()
             u.username = username
             u.email = email
-            u.password = password
+            u.password = generate_password_hash(password)
             u.register_date = datetime.datetime.now()
             for key, value in u.__dict__.items():
                 print(key, value, type(value))
@@ -98,7 +100,7 @@ def register():
     return flask.render_template('register.html',
                                  TITLE='Kayıt ol')
 
-admins = ["YeNLoR", "Mrwn", "asd"]
+moderators = [1, 2]
 
 @app.route('/profile', methods=['GET', 'POST'])
 @flask_login.login_required
@@ -114,27 +116,11 @@ def profile():
         new_password = request.form['new_password']
         password = request.form['password']
         user = db.session.query(Users).filter_by(id=flask_login.current_user.id).first()
-        if user.password == password and 16 >= len(new_password) >= 1:
-            user.password = new_password
+        if check_password_hash(user.password, password) and 16 >= len(new_password) >= 1:
+            user.password = generate_password_hash(new_password)
             db.session.add(user)
             db.session.commit()
 
-    if "delete_user" in request.form:
-        username = request.form['delete_user']
-        if flask_login.current_user.username == username:
-            for post in Posts.query.filter_by(author_id=username).all():
-                db.session.delete(post)
-            db.session.delete(Users.query.filter_by(username=username).first())
-            flask_login.logout_user()
-            db.session.commit()
-
-            return flask.redirect(flask.url_for('index'))
-
-    if "delete" in request.form:
-        post_id = request.form['delete']
-        if flask_login.current_user.username == db.session.query(Posts).filter_by(id=post_id).first().author_id:
-            db.session.delete(Posts.query.get(request.form["delete"]))
-            db.session.commit()
     return flask.render_template('profile.html',
                                  TITLE="Profil",
                                  user=flask_login.current_user,
@@ -144,30 +130,11 @@ def profile():
 
 @app.route('/profile/<id>', methods=['GET','POST'])
 def show_profile(id):
-    admin = False
     user = db.session.query(Users).filter_by(id=id).first()
-    if flask_login.current_user.username in admins:
-        admin = True
-    if "delete_user" in request.form:
-        username = request.form['delete_user']
-        if admin:
-            for post in Posts.query.filter_by(author_id=username).all():
-                db.session.delete(post)
-            db.session.delete(Users.query.filter_by(username=username).first())
-            flask_login.logout_user()
-            db.session.commit()
-            return flask.redirect(flask.url_for('index'))
-    if "delete" in request.form:
-        post_id = request.form['delete']
-        if admin:
-            db.session.delete(Posts.query.get(post_id))
-            db.session.commit()
-            return flask.redirect(request.referrer)
     return flask.render_template('profile.html',
                                 TITLE="Profil",
                                 user=user,
-                                posts=Posts.query.filter_by(author_id=id).all(),
-                                admin=admin)
+                                posts=Posts.query.filter_by(author_id=id).all())
 
 @app.route('/post', methods=['GET','POST'])
 @flask_login.login_required
@@ -210,6 +177,36 @@ def search_title(search):
     posts = Posts.query.filter(Posts.title.contains(search)).all()
     return flask.render_template('index.html',
                                  posts=posts)
+@app.route("/delete_post/", methods=['POST'])
+@flask_login.login_required
+def delete_post():
+    post = {}
+    moderator = False
+    if request.form["delete_post"]:
+        post_id = int(request.form["delete_post"])
+        post = Posts.query.filter_by(id=post_id).first()
+    if flask_login.current_user.id in moderators:
+        moderator = True
+    if moderator or post.author_id == flask_login.current_user.id:
+        db.session.delete(post)
+        db.session.commit()
+        return flask.redirect(request.referrer)
+
+@app.route("/delete_user/", methods=['POST'])
+@flask_login.login_required
+def delete_user():
+    user = {}
+    moderator = False
+    if request.form["delete_user"]:
+        user_id = int(request.form["delete_user"])
+        user = Users.query.filter_by(id=user_id).first()
+    print(user)
+    if flask_login.current_user.id in moderators:
+        moderator = True
+    if moderator or flask_login.current_user.id == user.id:
+        db.session.delete(user)
+        db.session.commit()
+        return flask.redirect(request.referrer)
 
 if __name__ == '__main__':
     with app.app_context():
